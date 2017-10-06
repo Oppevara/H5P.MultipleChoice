e9f7319deea0dba0757eb4f72194b4f3dfc7dae8
Karli Raudsepp <karliraudsepp@hazarada.htk.tlu.ee>
2017-10-06 12:06:25 +0300

    include guard fix

--- ckeditor_webloader.js
+++ ckeditor_webloader.js
@@ -1,6 +1,6 @@
 (function(){
-	if (window.GGB_LOADED === true) return;
-	window.GGB_LOADED = true;
+	if (window.MUB_CK === true) return;
+	window.MUB_CK = true;
 	var script = document.createElement("script");
 	script.src = "https://cdn.ckeditor.com/4.7.3/standard/ckeditor.js";
 	document.head.appendChild(script);
