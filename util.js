07ea0506df8883739c481b894985fb19020efb15
Karli Raudsepp <karliraudsepp@hazarada.htk.tlu.ee>
2017-12-21 15:06:21 +0200

    closes oppevara/oppevara/#63

--- util.js
+++ util.js
@@ -19,13 +19,43 @@ var random_string = function(n) {
 
 var h5p_get_data_obj = function(s) {
 	if (s === undefined) return undefined;
+	if (s.length > 0 && (s[0] == "[" || s[0] == "{")) {
+		return h5p_get_data_obj_v0(s);
+	}
+
+	if (s.length >= 3 && s.substring(0, 3) == "v1_") {
+		return h5p_get_data_obj_v1(s);
+	}
+
+	console.log("Corrputed or unknown data format");
+	return undefined;
+};
+
+
+var h5p_get_data_str = function(o) {
+	return h5p_get_data_str_v1(o);
+}
+
+var h5p_get_data_obj_v1 = function(s) {
+	return JSON.parse(atob(s.substring(3)));
+}
+
+var h5p_get_data_str_v1 = function(o) {
+	if (o === undefined) return undefined;
+	return "v1_" + btoa(JSON.stringify(o));
+};
+
+
+//	for historic reference
+var h5p_get_data_obj_v0 = function(s) {
 	s = s.replace(new RegExp(/&quot;/, 'g'), "\"");
 	s = s.replace(new RegExp(/&lt;/, 'g'), "<");
 	s = s.replace(new RegExp(/&gt;/, 'g'), ">");
+	console.log(s);
 	return JSON.parse(s);
-};
+}
 
-var h5p_get_data_str = function(o) {
+var h5p_get_data_str_v0 = function(o) {
 	if (o === undefined) return undefined;
 	return JSON.stringify(o);
-};
+}
