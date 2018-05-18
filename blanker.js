41d9b3c7b1d8a37336878d0863b7a51ca6591462
Karli Raudsepp <karliraudsepp@Karlis-MacBook-Pro.local>
2018-05-18 08:21:06 +0300

    close oppevara/oppevara#90

--- blanker.js
+++ blanker.js
@@ -30,7 +30,7 @@ var confirm_token = {
 //	sub and superscript support
 
 function to_unicode_superscript(s) {
-	var palette = { "0" : "⁰", "1":"¹", "2":"²","3":"³","4":"⁴","5":"⁵","6":"⁶","7":"⁷","8":"⁸","9":"⁹" };
+	var palette = { "0" : "⁰", "1":"¹", "2":"²","3":"³","4":"⁴","5":"⁵","6":"⁶","7":"⁷","8":"⁸","9":"⁹","-":"⁻","+":"⁺" };
 	var constr = "";
 	for (var i = 0; i < s.length; i++) {
 		if (s[i] in palette) constr += palette[s[i]];
@@ -40,7 +40,7 @@ function to_unicode_superscript(s) {
 }
 
 function to_unicode_subscript(s) {
-	var palette = { "0" : "₀", "1":"₁", "2":"₂","3":"₃","4":"₄","5":"₅","6":"₆","7":"₇","8":"₈","9":"₉" };
+	var palette = { "0" : "₀", "1":"₁", "2":"₂","3":"₃","4":"₄","5":"₅","6":"₆","7":"₇","8":"₈","9":"₉","-":"₋","+":"₊" };
 	var constr = "";
 	for (var i = 0; i < s.length; i++) {
 		if (s[i] in palette) constr += palette[s[i]];
